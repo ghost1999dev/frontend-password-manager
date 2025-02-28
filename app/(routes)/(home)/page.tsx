4c8faf7968a671351d8353ba4cee42038cd8a444
fernandoDev <blancogonzalezfernandoalexis@gmail.com>
2025-02-28 15:24:04 -0600

    init: uploading the firts project

---
+++ app/(routes)/(home)/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+
+export default function Home() {
+
+  const showMessage =()=>{
+    toast.success("Operacion exitosa")
+  }
+  return (
+    <div>
+      <Button onClick={showMessage }>Show message</Button>
+    </div>
+  );
+}
